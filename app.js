78078698f84916195a0122960db026b9fab7ccd1
herintsaina <herintsainarak024@gmail.com>
2023-08-09 12:46:17 +0300

    Changement picture mobile

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ passwordToggle.addEventListener('change', function() {
 
   if (password.type === 'password') {
     password.type = 'text'
-    passwordLabel.innerHTML = '<img class="h-full" src="./assets/images/icon/eye-open.png" alt="">'
+    passwordLabel.innerHTML = '<img class="h-full pt-1" src="./assets/images/icon/eye-open.png" alt="">'
   } else {
     password.type = 'password'
-    passwordLabel.innerHTML = '<img class="h-full" src="./assets/images/icon/eye-close.png" alt="">'
+    passwordLabel.innerHTML = '<img class="h-full pt-1" src="./assets/images/icon/eye-close.png" alt="">'
   }
 
   password.focus()
